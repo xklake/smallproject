3026ccddd7d062c9cbdbe918e54cf2008a763a03
Jone.Li <joneleesz@hotmail.com>
2015-05-27 17:01:05 +0100

    first version

---
+++ Product Importer/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Product_Importer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.imgFold = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sourceFile = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 501);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "生成家家优品导入文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "XLSX (*.xlsx)|*.xlsx|CSV File (*.csv)|*.csv|All files (*.*)|*.*";
+            // 
+            // imgFold
+            // 
+            this.imgFold.Location = new System.Drawing.Point(145, 67);
+            this.imgFold.Name = "imgFold";
+            this.imgFold.Size = new System.Drawing.Size(394, 20);
+            this.imgFold.TabIndex = 1;
+            this.imgFold.Text = "E:\\jiajiayoupin\\化妆品429";
+            this.imgFold.MouseDown += new System.Windows.Forms.MouseEventHandler(this.imgFold_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "图片文件夹";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "产品文件";
+            // 
+            // sourceFile
+            // 
+            this.sourceFile.Location = new System.Drawing.Point(145, 19);
+            this.sourceFile.Name = "sourceFile";
+            this.sourceFile.Size = new System.Drawing.Size(394, 20);
+            this.sourceFile.TabIndex = 3;
+            this.sourceFile.Text = "E:\\jiajiayoupin\\productslist.xlsx";
+            this.sourceFile.MouseDown += new System.Windows.Forms.MouseEventHandler(this.sourceFile_MouseDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(329, 501);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "生成淘宝图片";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(145, 111);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(394, 20);
+            this.txtOutput.TabIndex = 6;
+            this.txtOutput.Text = "E:\\jiajiayoupin\\";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "输入文件路径";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 588);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sourceFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imgFold);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox imgFold;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox sourceFile;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Product Importer/Form1.cs
@@ -0,0 +1,439 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO; 
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+
+namespace Product_Importer
+{
+    public partial class Form1 : Form
+    {
+        ArrayList inputRows = new ArrayList();
+
+        ArrayList skuList = new ArrayList();
+
+        ArrayList allImages = new ArrayList();
+
+        ArrayList outPut = new ArrayList();
+
+        string url = "http://7xiosd.com1.z0.glb.clouddn.com/"; 
+
+        //"ID","分类","名称","Sku","库存","重量","市场价","商城价","简述","内容","缩略图","图片","关键字","描述","类型","品牌","产地","发货地",缩略图集,图片集
+
+        string rowtemp = ",\"{cat}\",\"{name}\",\"{sku}\",\"{stock}\",\"{weight}\",\"{fullprice}\",\"{discountprice}\",\"{brief}\",\"{content}\",\"{smallimage}\",\"{largeimage}\",\"{keywords}\",\"{seodesc}\",\"17\",\"英国\",\"英国\",\"{brand}\",\"{smallimageset}\",\"{largeimageset}\"";
+
+
+        string head = "ID,分类,名称,Sku,库存,重量,市场价,商城价,简述,内容,缩略图,图片,关键字,描述,类型,品牌,产地,发货地,缩略图集,图片集"; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sourceFile_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                sourceFile.Text = openFileDialog1.FileName; 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Application.Exit();
+
+            inputRows.Clear();
+            skuList.Clear();
+            allImages.Clear();
+            outPut.Clear();
+
+
+            readSourceFile();
+
+            readImgfromDirectory();
+
+            processTaobao();
+
+            writeCSV();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            inputRows.Clear();
+            skuList.Clear();
+            allImages.Clear();
+            outPut.Clear(); 
+
+
+            readSourceFile();
+
+            readImgfromDirectory();
+
+            process();
+
+            writeCSV();
+
+        }
+
+
+        private void processTaobao()
+        {
+            allImages.Sort();
+
+            foreach (Object row in inputRows)
+            {
+                //rowtemp = ",\"{cat}\",\"{name}\",\"{sku}\",\"{stock}\",\"{weight}\",\"{fullprice}\",\"{discountprice}\",\"{brief}\",{content},\"{smallimage}\",\"{largeimage}\",\"{keywords}\",\"{seodesc}\",\"17\",\"英国\",\"英国\"，\"{brand}\"，\"{smallimageset}\",\"{largeimageset}\"";
+                rowtemp = ",\"{cat}\",\"{name}\",\"{enname}\",\"{sku}\",{content}";
+
+                ArrayList a = (ArrayList)row;
+
+                string sku = a[0].ToString();
+
+                string cat = a[a.Count - 1].ToString();
+
+                string[] categories = cat.Split('|');
+
+
+                {
+                    rowtemp = rowtemp.Replace("{sku}", a[0].ToString().Trim());
+
+                    rowtemp = rowtemp.Replace("{enname}", a[1].ToString().Trim());
+
+                    rowtemp = rowtemp.Replace("{name}", a[2].ToString().Trim());
+
+                    rowtemp = rowtemp.Replace("{weight}", a[4].ToString().Trim());
+
+                    rowtemp = rowtemp.Replace("{stock}", a[5].ToString().Trim());
+
+                    rowtemp = rowtemp.Replace("{brief}", a[3].ToString().Trim());
+
+                    rowtemp = rowtemp.Replace("{brand}", a[8].ToString().Trim());
+
+                    rowtemp = rowtemp.Replace("{fullprice}", a[6].ToString().Trim());
+
+                    rowtemp = rowtemp.Replace("{discountprice}", a[7].ToString().Trim());
+
+
+                    //ok, let's find out all the images 
+                    string detailimage = "";
+                    string smallimage = "";
+                    string largeimage = "";
+                    string smallimageset = "";
+                    string largeimageset = "";
+
+
+                    foreach (string img in allImages)
+                    {
+                        //large images
+                        if (img.IndexOf(sku + "-L-") != -1)
+                        {
+                            largeimageset += url + img + "|";
+
+                            if (largeimage.Length == 0)
+                            {
+                                largeimage = url + img;
+                            }
+                        }
+
+                        else if (img.IndexOf(sku + "-S-") != -1)
+                        {
+                            smallimageset += url + img + "|";
+
+                            if (smallimage.Length == 0)
+                            {
+                                smallimage = url + img;
+                            }
+                        }
+                        else if (img.IndexOf(sku) != -1)
+                        {
+                            detailimage += "<img src=\"" + url + img + "\"></img>";
+                        }
+                    }
+
+
+                    rowtemp = rowtemp.Replace("{smallimage}", smallimage);
+                    rowtemp = rowtemp.Replace("{largeimage}", largeimage);
+                    rowtemp = rowtemp.Replace("{smallimageset}", smallimageset);
+                    rowtemp = rowtemp.Replace("{largeimageset}", largeimageset);
+                    rowtemp = rowtemp.Replace("{content}", detailimage);
+                }
+
+
+                foreach (string str in categories)
+                {
+                    outPut.Add(rowtemp.Replace("{cat}", str));
+                }
+            }
+
+        }
+
+
+
+        private void process()
+        {
+            allImages.Sort(); 
+
+            foreach(Object row in inputRows)
+            {
+                rowtemp = ",\"{cat}\",\"{name}\",\"{sku}\",\"{stock}\",\"{weight}\",\"{fullprice}\",\"{discountprice}\",\"{brief}\",\"{content}\",\"{smallimage}\",\"{largeimage}\",\"{keywords}\",\"{seodesc}\",\"17\",\"英国\",\"英国\",\"{brand}\",\"{smallimageset}\",\"{largeimageset}\"";
+
+                ArrayList a = (ArrayList)row;
+
+                string sku = a[0].ToString();
+
+                string cat = a[a.Count - 1].ToString();
+
+                string [] categories = cat.Split('|');
+
+
+                {
+                    rowtemp = rowtemp.Replace("{sku}", a[0].ToString().Trim());
+
+                    rowtemp = rowtemp.Replace("{name}", a[2].ToString().Trim());
+
+                    rowtemp = rowtemp.Replace("{weight}", a[4].ToString().Trim());
+
+                    rowtemp = rowtemp.Replace("{stock}", a[5].ToString().Trim());
+
+                    rowtemp = rowtemp.Replace("{brief}", a[3].ToString().Trim());
+
+                    rowtemp = rowtemp.Replace("{brand}", a[8].ToString().Trim());
+
+                    rowtemp = rowtemp.Replace("{fullprice}", a[6].ToString().Trim());
+
+                    rowtemp = rowtemp.Replace("{discountprice}", a[7].ToString().Trim());
+
+
+                    //ok, let's find out all the images 
+                    string detailimage = "";
+                    string smallimage = "";
+                    string largeimage = ""; 
+                    string smallimageset = "";
+                    string largeimageset = ""; 
+
+
+                    foreach(string img in allImages)
+                    {
+                        //large images
+                        if(img.IndexOf(sku + "-L-") != -1)
+                        {
+                            largeimageset += url + img + "|";
+
+                            if (largeimage.Length == 0)
+                            {
+                                largeimage = url + img; 
+                            }
+                        }
+
+                        else if(img.IndexOf(sku + "-S-") != -1)
+                        {
+                            smallimageset += url + img + "|";
+
+                            if (smallimage.Length == 0)
+                            {
+                                smallimage = url + img; 
+                            }
+                        }
+                        else if(img.IndexOf(sku) != -1)
+                        {
+                            detailimage += "<img src=\"\"" + url + img + "\"\"></img>|"; 
+                        }
+                    }
+
+
+                    rowtemp = rowtemp.Replace("{smallimage}", smallimage);
+                    rowtemp = rowtemp.Replace("{largeimage}", largeimage);
+                    rowtemp = rowtemp.Replace("{smallimageset}", smallimageset);
+                    rowtemp = rowtemp.Replace("{largeimageset}", largeimageset);
+                    rowtemp = rowtemp.Replace("{content}", detailimage);
+                }
+
+
+                foreach(string str in categories)
+                {
+                    outPut.Add(rowtemp.Replace("{cat}", str));
+                }
+            }
+
+        }
+
+
+        private void writeCSV()
+        {
+
+            FileInfo fi = new FileInfo(txtOutput.Text.Trim()); 
+
+      
+          if(!fi.Exists)
+          {
+              fi.Directory.Create();
+          }
+
+          try {
+                  FileStream  fs = new FileStream(txtOutput.Text.Trim() + "\\" + DateTime.Now.ToString("yyyyMMdd-HHmmss") + ".csv", System.IO.FileMode.Create, System.IO.FileAccess.Write);
+                  StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8);
+
+                    sw.WriteLine(head);
+
+                    foreach(string row in outPut)
+                    {
+                        sw.WriteLine(row); 
+                    }
+
+                  sw.Close();
+                  fs.Close();
+              }
+            catch(Exception x)
+          {
+              
+              MessageBox.Show("写入文件失败， 失败的原因是"+ x.Message.ToString()); 
+          }
+
+        }
+
+        private void readImgfromDirectory()
+        {
+            if(imgFold.Text.Length ==0)
+            {
+                return; 
+            }
+
+            ProcessDirectory(imgFold.Text);  
+        }
+
+
+        private void ProcessDirectory(string dir)
+        {
+            string[] files = Directory.GetFiles(dir);
+
+            foreach (string str in files)
+            {
+                int k = str.LastIndexOf("\\"); 
+
+                allImages.Add(str.Substring(k + 1)); 
+            }
+
+            string[] subdirectoryEntries = Directory.GetDirectories(dir);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+
+
+        private void imgFold_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                imgFold.Text = folderBrowserDialog1.SelectedPath; 
+            }
+        }
+
+        
+        public bool readSourceFile()
+        {
+            if(sourceFile.Text.Length == 0)
+            { 
+                return false; 
+            }
+
+
+            // load excel application 
+
+            ReadExcelFile(); 
+
+            return true; 
+        }
+
+
+
+
+        private void ReadExcelFile()
+        {
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            Excel.Range range;
+
+            Object cellvalue;
+            int rCnt = 0;
+            int cCnt = 0;
+
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open(sourceFile.Text, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            range = xlWorkSheet.UsedRange;
+
+
+            //clear old data
+            inputRows.Clear(); 
+
+
+            // start from second line
+            for (rCnt = 2; rCnt <= range.Rows.Count; rCnt++)
+            {
+                ArrayList row = new ArrayList(); 
+                for (cCnt = 1; cCnt <= range.Columns.Count; cCnt++)
+                {
+                    cellvalue = (range.Cells[rCnt, cCnt] as Excel.Range).Value2;
+
+
+                    if (cellvalue != null)
+                    {
+                        row.Add(cellvalue.ToString());
+                        Console.WriteLine(cellvalue.ToString());
+
+                        if (cCnt == 1)
+                        {
+                            skuList.Add(cellvalue.ToString().Trim());
+                        }
+                    }
+                }
+
+                if(row.Count >0)
+                { 
+                    inputRows.Add(row);
+                }
+            }
+
+
+            xlWorkBook.Close(true, null, null);
+            xlApp.Quit();
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+        }
+
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Unable to release the Object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        } 
+
+
+
+    }
+}
